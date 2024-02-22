63cdd17289ca40c9547f8ea5523c93b7c7f2338b
AdityaNG <AdityaNG@users.noreply.github.com>
2024-02-22 15:14:02 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="AdityaNG"
+original_name="drivellava"
+original_urlname="DriveLLaVA"
+original_description="Awesome drivellava created by AdityaNG"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv drivellava $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
