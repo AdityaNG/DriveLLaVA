d38dbca83bbdf95e5dea9959611268b5b019786d
Aditya NG <adityang5@gmail.com>
2024-02-24 10:50:57 +0530

    refactor(drivellava/datasets/): pose dataset

---
+++ drivellava/datasets/commavq.py
@@ -0,0 +1,114 @@
+import os
+
+import numpy as np
+
+from drivellava.utils import (
+    remove_noise,
+)
+
+class CommaVQPoseDataset:
+    def __init__(
+        self,
+        pose_path: str,
+        num_frames: int = 20,
+        window_length: int = 21 * 2 - 1,
+        polyorder: int = 1,
+    ):
+        assert os.path.exists(pose_path), pose_path
+        self.pose_path = pose_path
+        self.num_frames = num_frames
+        pose = np.load(pose_path)
+        
+        # Inches to meters
+        pose[:, :3] *= 0.0254
+        pose[:, 1] = 0.0
+
+        pose = remove_noise(
+            pose,
+            window_length=window_length,
+            polyorder=polyorder,
+        )
+
+        pose[np.isnan(pose)] = 0
+        pose[np.isinf(pose)] = 0
+
+        self.pose = pose
+    
+    def __len__(self):
+        return len(self.pose)
+    
+    def __getitem__(self, index):
+        sub_pose = get_local_pose(
+            self.pose,
+            index,
+            self.num_frames,
+        )
+
+        trajectory = sub_pose[:, :3].copy()
+
+        # Swap axes
+        trajectory = trajectory[:, [2, 1, 0]]
+
+        return trajectory
+
+def get_local_pose(
+    pose: np.ndarray,
+    frame_index: int,
+    num_frames: int,
+):
+    """
+    Gets the pose from a specific time index
+
+    Rotates and translates the pose to the correct position
+    """
+    # pose: (1200, 6)
+    assert pose.shape[0] == 1200
+    assert pose.shape[0] - frame_index > num_frames
+
+    sub_pose = pose[frame_index : frame_index + num_frames].copy()
+
+    # origin = sub_pose[0]
+    sub_pose[0, :] = 0
+
+    # Each element is (x, y, z, roll, pitch, yaw)
+    for i in range(1, sub_pose.shape[0]):
+        rot = get_rotation_matrix(sub_pose[i])
+        sub_pose[i, :3] += np.dot(sub_pose[:, :3], rot)[i - 1, :3]
+
+    sub_pose[1:, :3] += sub_pose[:-1, :3]
+
+    return sub_pose
+
+def get_rotation_matrix(pose):
+    """
+    origin: (x, y, z, roll, pitch, yaw)
+    """
+    # Construct 3D rotation matrix
+    origin = pose.copy()
+
+    rot_x = np.array(
+        [
+            [1, 0, 0],
+            [0, np.cos(origin[3]), -np.sin(origin[3])],
+            [0, np.sin(origin[3]), np.cos(origin[3])],
+        ]
+    )
+    rot_y = np.array(
+        [
+            [np.cos(origin[4]), 0, np.sin(origin[4])],
+            [0, 1, 0],
+            [-np.sin(origin[4]), 0, np.cos(origin[4])],
+        ]
+    )
+    rot_z = np.array(
+        [
+            [np.cos(origin[5]), -np.sin(origin[5]), 0],
+            [np.sin(origin[5]), np.cos(origin[5]), 0],
+            [0, 0, 1],
+        ]
+    )
+
+    rot = np.dot(rot_z, np.dot(rot_y, rot_x))
+
+    return rot
+
--- drivellava/scripts/visualize_pose.py
+++ drivellava/scripts/visualize_pose.py
@@ -12,71 +12,9 @@ from drivellava.constants import DECODED_IMGS_ALL_AVAILABLE
 from drivellava.utils import (
     plot_bev_trajectory,
     plot_steering_traj,
-    remove_noise,
 )
 
-
-def get_rotation_matrix(pose):
-    """
-    origin: (x, y, z, roll, pitch, yaw)
-    """
-    # Construct 3D rotation matrix
-    origin = pose.copy()
-
-    rot_x = np.array(
-        [
-            [1, 0, 0],
-            [0, np.cos(origin[3]), -np.sin(origin[3])],
-            [0, np.sin(origin[3]), np.cos(origin[3])],
-        ]
-    )
-    rot_y = np.array(
-        [
-            [np.cos(origin[4]), 0, np.sin(origin[4])],
-            [0, 1, 0],
-            [-np.sin(origin[4]), 0, np.cos(origin[4])],
-        ]
-    )
-    rot_z = np.array(
-        [
-            [np.cos(origin[5]), -np.sin(origin[5]), 0],
-            [np.sin(origin[5]), np.cos(origin[5]), 0],
-            [0, 0, 1],
-        ]
-    )
-
-    rot = np.dot(rot_z, np.dot(rot_y, rot_x))
-
-    return rot
-
-
-def get_local_pose(
-    pose: np.ndarray,
-    frame_index: int,
-    num_frames: int,
-):
-    """
-    Gets the pose from a specific time index
-
-    Rotates and translates the pose to the correct position
-    """
-    # pose: (1200, 6)
-    assert pose.shape[0] == 1200
-    assert pose.shape[0] - frame_index > num_frames
-
-    sub_pose = pose[frame_index : frame_index + num_frames].copy()
-
-    # origin = sub_pose[0]
-    sub_pose[0, :] = 0
-
-    # Each element is (x, y, z, roll, pitch, yaw)
-    for i in range(1, sub_pose.shape[0]):
-        rot = get_rotation_matrix(sub_pose[i])
-        sub_pose[i, :3] += np.dot(sub_pose[:, :3], rot)[i - 1, :3]
-
-    sub_pose[1:, :3] += sub_pose[:-1, :3]
-
-    return sub_pose
+from drivellava.datasets.commavq import CommaVQPoseDataset
 
 
 def main():
@@ -92,55 +30,20 @@ def main():
         )
         print(pose_path)
 
-        assert os.path.exists(pose_path), pose_path
-
-        pose = np.load(pose_path)
-
-        # Inches to meters
-        pose[:, :3] *= 0.0254
-        pose[:, 1] = 0.0
-        # pose[:, 2] *= 0.01
-
-        # pose[1:, :3] += pose[:-1, :3]
-
-        # pose[:, :3] = smoothen_traj(pose[:, :3])
-        # pose = smoothen_traj(pose, window_size=100)
-        # pose = smoothen_traj(pose, window_size=20*3)
-        pose = remove_noise(
-            pose,
-            window_length=21 * 2 -1,
-            # window_length=9,
+        pose_dataset = CommaVQPoseDataset(
+            pose_path,
+            num_frames=NUM_FRAMES,
+            window_length=21 * 2 - 1,
             polyorder=1,
         )
-
-        pose[np.isnan(pose)] = 0
-        pose[np.isinf(pose)] = 0
-
-        print("pose", pose.shape, pose.dtype)
-
-        for i in range(6):
-            print(
-                f"pose[:,{i}]",
-                (np.min(pose[:, i]), np.max(pose[:, i])),
-                np.mean(pose[:, i]),
-                np.std(pose[:, i]),
-            )
 
         # Iterate over the embeddings in batches and decode the images
         for i in tqdm(
             range(0, len(decoded_imgs_list) - NUM_FRAMES, 1), desc="Video"
         ):
             img = cv2.imread(decoded_imgs_list[i])
-            sub_pose = get_local_pose(
-                pose,
-                i,
-                NUM_FRAMES,
-            )
 
-            trajectory = sub_pose[:, :3]
-
-            # Swap axes
-            trajectory = trajectory[:, [2, 1, 0]]
+            trajectory = pose_dataset[i]
 
             print(
                 "trajectory[0]",
